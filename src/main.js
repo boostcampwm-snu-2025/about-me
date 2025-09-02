8140936ef1813452293caa90203757e6507624d1
lightsaber_notebook <bestornot04@naver.com>
2025-09-02 19:53:36 +0900

    .

---
+++ src/main.js
@@ -0,0 +1,5 @@
+// main.js
+const appDiv = document.getElementById("app");
+const message = document.createElement("p");
+message.textContent = "Hello World from JavaScript!";
+appDiv.appendChild(message);
