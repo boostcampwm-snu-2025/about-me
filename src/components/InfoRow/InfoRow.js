80e27a221bc7784ce89a2b4c940331f75e2ca6e1
DESKTOP-9N569U1\SAMSUNG <seojoonrp@snu.ac.kr>
2025-09-03 22:53:27 +0900

    feature: deleted answer logic

--- src/components/InfoRow/InfoRow.js
+++ src/components/InfoRow/InfoRow.js
@@ -2,11 +2,26 @@
 
 import "./InfoRow.css";
 
+// 물결(~)로 감싸진 부분을 감지해 취소선 속성을 넣는 함수
+const formatAnswer = (answer) => {
+  const answerParts = answer.split("~");
+
+  const formattedParts = answerParts.map((part, index) => {
+    if (index % 2 === 1) {
+      return /*html*/ `<del class="info-row__deleted">${part}</del>`;
+    } else {
+      return part;
+    }
+  });
+
+  return formattedParts.join("");
+};
+
 const InfoRow = ({ question, answer }) => {
   return /*html*/ `
     <div class="info-row">
       <p class="info-row__question">${question}</p>
-      <p class="info-row__answer">${answer}</p>
+      <p class="info-row__answer">${formatAnswer(answer)}</p>
     </div>
   `;
 };
--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ const appHTML = /*html*/ `
   <section class="info-card info-card__hobby">
     ${InfoRow({
       question: "취미가 뭔가요?",
-      answer: "해외축구 시청\n시각디자인\n알고리즘 문제풀기\n등등등",
+      answer: "해외축구 시청\n시각디자인\n~알고리즘 문제풀기~\n등등등",
     })}
     ${InfoRow({ question: "뭘 좋아하시나요", answer: "고양이 좋아합니다." })}
     ${InfoRow({ question: "가장 좋아하는 색?", answer: "파란색 좋아합니다." })}
