035e0013f152472c80044edfe3068d1a3b1a0ba2
DESKTOP-9N569U1\SAMSUNG <seojoonrp@snu.ac.kr>
2025-09-03 21:19:05 +0900

    basic layout (no css)

---
+++ src/components/InfoRow/InfoRow.js
@@ -0,0 +1,12 @@
+/* InfoRow.js */
+
+const InfoRow = ({ question, answer }) => {
+  return /*html*/ `
+    <div class="info-row">
+      <p class="info-row__question">${question}</p>
+      <p class="info-row__answer">${answer}</p>
+    </div>
+  `;
+};
+
+export default InfoRow;
