87ec850a7cdf390166defdaab9fb6ce01bb015c7
shsha0110 <shsh0201@snu.ac.kr>
2025-09-06 21:19:44 +0900

    feat(profile-card): Add back face structure

---
+++ src/profile_card_back.js
@@ -0,0 +1,10 @@
+// 프로필 카드 바탕
+const profile_card_container = document.createElement("div");
+const profile_card = document.createElement("div");
+
+profile_card_container.className = "profile-card-container";
+profile_card.className = "profile-card";
+
+document.body.appendChild(profile_card_container);
+profile_card_container.appendChild(profile_card);
+
