72adaf07f4efd1e4e4e094ef1122636b3e154ed3
shsha0110 <shsh0201@snu.ac.kr>
2025-09-06 13:50:13 +0900

    feat: Add profile card container

---
+++ src/profile_card.js
@@ -0,0 +1,6 @@
+const profile_card_container = document.createElement("div");
+const profile_card = document.createElement("div");
+profile_card_container.className = "profile-card-container";
+profile_card.className = "profile-card";
+document.body.appendChild(profile_card_container);
+profile_card_container.appendChild(profile_card);
