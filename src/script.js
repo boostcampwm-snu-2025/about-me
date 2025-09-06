9d268ae5bbd9fd57a1711b142ee38a2d2e8b66e3
lchanss <lchans0319@gmail.com>
2025-09-06 12:46:03 +0900

    feat: 셔틀콕 랜덤 생성

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const detailsSectionNode = document.querySelector(".details-section");
 const countNode = document.querySelector(".count-section .count");
 const countButtonNode = document.querySelector(".count-section .count-button");
+const bodyNode = document.querySelector("body");
 
 const details = [
   { label: "이름", value: "임찬솔" },
@@ -9,6 +10,48 @@ const details = [
   { label: "관심 분야", value: "웹 프론트엔드 개발" },
 ];
 let count = 0;
+const shuttlecocks = [];
+
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function hideShuttlecock(shuttlecockNode) {
+  shuttlecockNode.style.display = "none";
+}
+
+function createRandomShuttlecock() {
+  const div = document.createElement("div");
+  div.classList.add("shuttlecock");
+
+  div.style.width = "50px";
+  div.style.height = "50px";
+  div.style.position = "absolute";
+
+  const minX = 0;
+  const minY = 0;
+  const maxX = window.innerWidth - 100; // 화면 너비 - 100px
+  const maxY = window.innerHeight - 100; // 화면 높이 - 100px
+
+  const x = getRandomNumber(minX, maxX);
+  const y = getRandomNumber(minY, maxY);
+
+  div.style.left = `${x}px`;
+  div.style.top = `${y}px`;
+
+  bodyNode.appendChild(div);
+  shuttlecocks.push(div);
+
+  executeRandomly(() => hideShuttlecock(div), 3000, 8000);
+  executeRandomly(createRandomShuttlecock, 2000, 5000);
+}
+
+function executeRandomly(actionFunc, minInterval, maxInterval) {
+  const randomDelay = getRandomNumber(minInterval, maxInterval);
+  setTimeout(() => {
+    actionFunc();
+  }, randomDelay);
+}
 
 // 상세 정보들 표시
 function displayDetails() {
@@ -51,6 +94,7 @@ function displayCount() {
 function handleCountButtonClick() {
   increaseCount();
   displayCount();
+  createRandomShuttlecock();
 }
 
 // 셔틀콕 우클릭 시 개수 감소
@@ -63,4 +107,8 @@ function handleCountButtonRightClick(e) {
 countButtonNode.addEventListener("click", handleCountButtonClick);
 countButtonNode.addEventListener("contextmenu", handleCountButtonRightClick);
 
-displayDetails();
+function initialize() {
+  displayDetails();
+  executeRandomly(createRandomShuttlecock, 0, 5000);
+}
+initialize();
