f858500783a29a88140d129ec39b5ea268c5e718
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-09-06 16:19:12 +0900

    ✨ 프로젝트 추가

--- week1-about-me/src/project-list.js
+++ week1-about-me/src/project-list.js
@@ -20,6 +20,32 @@ const PROJECTS = [
     ],
   },
   {
+    title: "키워드 및 이상형 기반 애니메이션 캐릭터 추천",
+    timeline: "2024년 여름",
+    position: "팀장, 프론트엔드, 디자인",
+    introduce:
+      "SKYST 2기 해커톤에서 제작한 프로젝트로, 총 100명 중 2등상을 수상하였습니다. 자신의 이상형과 똑닮은 애니메이션 캐릭터 또는 버츄얼 유튜버를 소개합니다. 서로 다른 차원에서의 사랑을 경험해보세요. ",
+    links: {
+      github: "https://github.com/Pironeer-APP/Pironeer_Attend_Web_Client",
+      presentation:
+        "https://drive.google.com/file/d/1WSwTzz_1GWgSoMKOv0_UJwrBAad-2TXB/view?usp=drive_link",
+      demo: "https://youtube.com/shorts/PF1ulFbeA2s?si=v9wPH183NkQVmt6W",
+    },
+    technologies: ["React", "JavaScript", "CSS Module"],
+  },
+  {
+    title: "쉐낏투유",
+    timeline: "2025년 여름",
+    position: "프론트엔드",
+    introduce:
+      'SKYST 3기 해커톤에서 제작한 프로젝트로, 총 100명 중 3등상을 수상하였습니다. 신해철의 "그대에게"에 어울리는 서비스를 개발하는 것이 해커톤의 주제였습니다. "그대에게"가 표현하는 청춘의 열정, 응원을 서비스로 표현하였습니다. 각 대학별로 그대에게 리듬에 맞춰 핸드폰을 흔들면 학교별 점수가 집계되는 학교 대항 리듬게임을 개발하였습니다.',
+    links: {
+      github:
+        "https://github.com/hwanheejung/yonsei-milk-waffle-web?tab=readme-ov-file",
+    },
+    technologies: ["React", "TypeScript", "Tailwindcss", "Vercel"],
+  },
+  {
     title: "동아리 출석 및 과제 확인 웹사이트",
     timeline: "2024년 6월",
     position: "프론트엔드, 디자인",
@@ -48,7 +74,7 @@ const createProjectCard = (project, index) => {
     .filter(([_, url]) => url.trim().length !== 0)
     .map(([type, url]) => {
       const linkText = linkTypeMap[type] || type;
-      return `<a href="${url}" class="${type}-link button" target="_blank">${linkText}</a>`;
+      return `<a href="${url}" class="${type}-link button" target="_blank" norefferer>${linkText}</a>`;
     })
     .join("");
 
