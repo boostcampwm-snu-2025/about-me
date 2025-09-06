861fbf35a60f60354361af8010c994e36c6793ce
Hyungjin Yi <hyungjin1120@naver.com>
2025-09-06 18:13:21 +0900

    나를 소개하기

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+const namecard1 = document.getElementById("namecard1")
+const namecard2 = document.getElementById('namecard2')
+
+namecard1.addEventListener('click', function() {
+    namecard1.style.display = 'none';
+    namecard2.style.display = 'flex';
+})
+
+namecard2.addEventListener('click', function() {
+    namecard2.style.display = 'none';
+    namecard1.style.display = 'flex';
+})
