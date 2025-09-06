493ec01b96f8868991e9ebcef4fb50f6599ea770
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-09-06 09:44:48 +0900

    ✨ 자기소개 슬롯 섹션 생성

--- week1-about-me/src/check-is-visible.js
+++ week1-about-me/src/check-is-visible.js
@@ -1,19 +1,31 @@
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("visible");
-      } else {
-        entry.target.classList.remove("visible");
-      }
-    });
-  },
-  {
-    rootMargin: "-10px",
-  }
-);
+const checkIsVisible = (visibleCallback, invisibleCallback) => {
+  return new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          visibleCallback(entry);
+          entry.target.classList.add("visible");
+        } else {
+          invisibleCallback(entry);
+          entry.target.classList.remove("visible");
+        }
+      });
+    },
+    {
+      rootMargin: "-10px",
+    }
+  );
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   const fadeElements = document.querySelectorAll(".fade-in-element");
+  const visibleCallback = (entry) => {
+    entry.target.classList.add("visible");
+  };
+  const invisibleCallback = (entry) => {
+    entry.target.classList.remove("visible");
+  };
+
+  const observer = checkIsVisible(visibleCallback, invisibleCallback);
   fadeElements.forEach((el) => observer.observe(el));
 });
