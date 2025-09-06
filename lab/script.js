be913959edecb92506020e8c3d1a53e40a8bbde7
Tapha-K <aidu2002@naver.com>
2025-09-06 15:23:50 +0900

    feat: 사진 더 보기 버튼 추가

--- lab/script.js
+++ lab/script.js
@@ -1,8 +1,26 @@
+const button = document.getElementById('load');
+const cats = document.querySelectorAll('.cat .hidden');
+
+let order = 0;
+
+function update() {     // 숨겨져 있던 사진을 클래스에서 제외하는 함수
+    if (order < cats.length) {
+        cats[order].classList.remove('hidden');
+        order++;
+    }
+    if (order >= cats.length) {
+        button.style.display = 'none';
+    }
+}
+
+button.addEventListener('click', update);
+
+
 const time = document.getElementById('time-scouter');
 
 const birth = new Date('2002-05-25');
 
-function counter() {
+function counter() {        // 태어난 날로부터 시간 계산하는 함수
     const current = new Date();
     const diff = current - birth;
     const day = Math.floor(diff / 1000 / 60 / 60 / 24);
