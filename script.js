39a42954a537c0c0963891585a4a9823e6e3f6bb
sumin <smkl1004@snu.ac.kr>
2025-09-07 14:34:10 +0900

    feat: 사이드바 클릭 시 본문 내용 변경 기능 구현

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.getElementById("external").addEventListener("click", function () {
-  alert("Hello World");
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll(".sidebar a");
+  const mainContent = document.getElementById("main-content");
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const content = link.getAttribute("data-content");
+      mainContent.innerHTML = `
+        <h2>저는</h2>
+        <p>${content}</p>
+      `;
+    });
+  });
 });
