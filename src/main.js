106cd6e1e26151f8bd9643710b246230e7425bb7
joo <122433286+kepten31415926@users.noreply.github.com>
2025-09-07 19:11:23 +0900

    made function for the grid thing. [to fix: order of project cards showing....] [misc. to do: add tag in project cards, animate hovering...]

--- src/main.js
+++ src/main.js
@@ -1,28 +1,57 @@
 const likes = [
     {title: "Adventure Time", tag: "tv-shows", img: "https://upload.wikimedia.org/wikipedia/en/3/37/Adventure_Time_-_Title_card.png"},
     {title: "Radiohead", tag: "artists", img: "https://upload.wikimedia.org/wikipedia/en/1/14/Inrainbowscover.png"},
-    {title: "Grand Budapest Hotel", tag: "movies"}
+    {title: "연어초밥", tag: "food", img: "https://sitem.ssgcdn.com/11/30/58/item/1000053583011_i1_750.jpg"},
+    {title: "Ethel Cain", tag: "artists", img: "https://upload.wikimedia.org/wikipedia/en/7/74/Preachers_daughter_ethel_cain.png"},
+    {title: "The Lego Movie", tag: "movies", img: "https://upload.wikimedia.org/wikipedia/en/1/10/The_Lego_Movie_poster.jpg"},
+    {title: "The Wind-Up Bird Chronicle", tag: "books", img: "https://upload.wikimedia.org/wikipedia/en/9/9f/Wind-up_Bird_Chronicle.jpg"},
+    {title: "", tag: "movies", img: ""},
+    {title: "", tag: "movies", img: ""},
+    {title: "", tag: "movies", img: ""},
+    {title: "", tag: "movies", img: ""},
+    {title: "", tag: "movies", img: ""},
+    {title: "", tag: "movies", img: ""},
+    {title: "", tag: "movies", img: ""}
+
 ];
 
-const grid = document.querySelector(".project-grid")
-likes.forEach(item =>{
-    const card = document.createElement("div");
-    card.classList.add("project-card");
-    card.dataset.tag = item.tag;
-    if (item.img){
-        const img = document.createElement("img");
-        img.src = item.img;
-        img.alt=item.title;
-        card.appendChild(img);
-    }
-    const title = document.createElement("h2");
-    title.textContent = item.title;
-    card.appendChild(title);
-    grid.appendChild(card);
-})
+const projectList = [
+    {title: "가지도 않을 표를 사는 사람들", url: "https://zoo-zer0.github.io/tickets-mid/", tag: "data-visualization,interactive", img: "https://zoo-zer0.github.io/img/project/ticket.png", description: "Translation: <em>Those Who Buy Tickets They Never Intend to Use</em><br><br>We investigated Korea’s secondary ticket market by reverse-engineering Ticket Bay and building a daily crawler to track listings. Our analysis uncovered how a small group of resellers dominate the market, inflating prices far beyond face value. The interactive site visualizes these patterns, letting you explore resale price gaps by game and even down to seat level.<br><br><em>Python, HTML, CSS, Javascript, D3.js</em></p>"},
+    {title: "Cold Plunges for Him: When the For You Page Isn't for Me", url:"https://zoo-zer0.github.io/project5/", tag: "data-visualization,writing", img: "https://zoo-zer0.github.io/img/project/cold%20plunge.jpg", description: "An analysis of 212 PubMed articles revealed that most cold therapy studies excluded women, yet this bias disappears as findings spread into health media. This project traces how advice based on male physiology gets generalized into universal wellness claims.<p><em>Python, HTML, CSS, Javascript, D3.js</em></p>"},
+    {title: "The SNU Quill 82nd Edition", url: "https://snuquill-public-s3.s3.ap-northeast-2.amazonaws.com/magazine/82.pdf", tag: "writing", img: "https://zoo-zer0.github.io/img/project/snuquill.png", description: "          Served as Editor-in-Chief of the 82nd edition of Seoul National University’s English magazine. Oversaw reporting, editing, and design to publish stories on campus politics, culture, and international affairs."},
+];
+function generateCards(containerSelector, items){
+    const grid = document.querySelector(containerSelector);
+    grid.innerHTML = "";
+    items.forEach(item=>{
+        const link = document.createElement("a");
+        link.href = item.url || "#";
+        link.target = "_blank";
+        const card = document.createElement("div");
+        card.classList.add("project-card");
+        card.dataset.tag = item.tag || "";
+        if (item.img){
+            const img = document.createElement("img");
+            img.src = item.img;
+            img.alt=item.title;
+            card.appendChild(img);
+        }
+        const title = document.createElement("h2");
+        title.textContent = item.title;
+        card.appendChild(title);
+        if (item.description){
+            const desc = document.createElement("p");
+            desc.innerHTML=item.description;
+            card.appendChild(desc);
+        }
+        link.appendChild(card);
+        grid.appendChild(link); //important!! wrap the card with link and add link to grid
+    })
+}
+generateCards(".likes .project-grid", likes);
+generateCards(".projects .project-grid", projectList);
 
 const tags = document.querySelectorAll(".tag");
-const projects = document.querySelectorAll('.project-card');
 
 tags.forEach(tagBtn => {
     tagBtn.addEventListener('click',()=>{
@@ -30,7 +59,7 @@ tags.forEach(tagBtn => {
         tagBtn.classList.add('active');
 
         const selectedTag = tagBtn.dataset.tag;
-
+        const projects = document.querySelectorAll(".project-card");
         projects.forEach(project=>{
             const projectTag = project.dataset.tag;
             if (selectedTag === 'all'|| projectTag.includes(selectedTag)){
