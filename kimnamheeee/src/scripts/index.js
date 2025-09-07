07caa6d33c0468ca7de94accf5034ad7198ec576
kimnamheeee <nami200284@snu.ac.kr>
2025-09-07 17:25:11 +0900

    ✨ feat(index): add window mapping to folder dbl click event

--- kimnamheeee/src/scripts/index.js
+++ kimnamheeee/src/scripts/index.js
@@ -1,26 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
+const folderToWindowMap = {
+    'about me': 'about-me-window',
+    'contact me!': 'contact-window',
+};
+  
+  const folderToUrlMap = {
+    'github': 'https://github.com/kimnamheeee',
+    'email': 'mailto:nami200284@snu.ac.kr',
+    'linkedin': 'https://www.linkedin.com/in/nam-hee-kim-754b16381',
+    'blog': 'https://velog.io/@knheeeee/posts',
+};
+  
+  document.addEventListener('DOMContentLoaded', () => {
     document.body.addEventListener('folder-double-click', (e) => {
-        const folderText = e.detail.text;
-        let url = null;
-
-        switch(folderText.toLowerCase()) {
-            case 'github':
-            url = 'https://github.com/kimnamheeee';
-            break;
-            case 'email':
-            url = 'mailto:nami200284@snu.ac.kr';
-            break;
-            case 'linkedin':
-            url = 'https://www.linkedin.com/in/nam-hee-kim-754b16381';
-            break;
-            case 'blog':
-            url = 'https://velog.io/@knheeeee/posts';
-            break;
-        }
-
-        if (url) {
-            window.open(url, '_blank');
-        }
+      const folderText = e.detail.text.trim().toLowerCase();
+  
+      const url = folderToUrlMap[folderText];
+      if (url) window.open(url, '_blank');
+  
+      const windowId = folderToWindowMap[folderText];
+      if (windowId) {
+        const win = document.getElementById(windowId);
+        win?.open();
+      }
     });
 });
   
